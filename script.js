19070311423ee6b42b4ec28e464e988285eca8d2
JohannSam <johannjoshiah@gmail.com>
2021-11-16 17:16:43 +0530

    first

---
+++ script.js
@@ -0,0 +1,23 @@
+var myVideo = document.getElementById("video"); 
+
+function playPause() {
+    if(myVideo.paused) {
+        myVideo.play();
+    }
+
+    else {
+        myVideo.pause();
+    }
+}
+
+function makeBig() {
+    myVideo.width = 1000;
+}
+
+function makeSmall() {
+    myVideo.width = 200; 
+}
+
+function makeNormal() {
+   myVideo.width = 400;
+}
